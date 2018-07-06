d474ef6d60cd77050dd3ef5147c2ffeb96771b28
williambl <william@williambl.com>
2018-07-06 17:30:07 +0100

    Added a simple webserver to redirect to steam To get around discord not allowing title-link to use steam:// links

--- generate-message.py
+++ generate-message.py
@@ -7,4 +7,4 @@ image = ""
 ni.ifaddresses('eth0')
 ip = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
 
-subprocess.run(["python3", "push-webhook.py", "--game", game, "--link", f"http://{ip}", "--addr", ip, "--image", image])
+subprocess.run(["python3", "push-webhook.py", "--game", game, "--link", f"http://{ip}:8080", "--addr", ip, "--image", image])
