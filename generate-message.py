592a45b5cc55c14f78f282ea92710dc7a498e2d4
williambl <william@williambl.com>
2018-07-06 17:13:34 +0100

    Added a script to generate messages to send

---
+++ generate-message.py
@@ -0,0 +1,10 @@
+import netifaces as ni
+import subprocess
+
+game = ""
+image = ""
+
+ni.ifaddresses('eth0')
+ip = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
+
+subprocess.run(["python3", "push-webhook.py", "--game", game, "--link", f"http://{ip}", "--addr", ip, "--image", image])
