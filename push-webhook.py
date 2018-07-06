e666deac992a45dbbcd1ecb3efdc5b88eca03414
williambl <william@williambl.com>
2018-07-06 15:46:11 +0100

    Added more information to the message

--- push-webhook.py
+++ push-webhook.py
@@ -2,20 +2,32 @@ import requests
 
 url = ""
 
-name = "A name"
 game = "A game"
+link = "https://example.com"
 addr = "0.0.0.0"
+image = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Example_image.svg/1024px-Example_image.svg.png"
 
 request = {
     "text": "New server started!",
     "attachments": [
         {
-            "fallback": "New "+game+" server started on "+addr,
-            "author_name": name,
-            "title": "New "+game+" server started!",
-            "text": "Address: "+addr
+            "fallback": f"New {game} server started at {addr}",
+            "title": f"{game} server started!",
+            "title_link": f"{link}",
+            "text": f"Address: "+addr,
+            "color": "#fa8423",
+            "fields": [
+                {
+                    "title": "Game",
+                    "value": f"{game}"
+                },
+		{
+		    "title": "Address",
+		    "value": f"{addr}"
+		}
+            ],
+            "image_url": f"{image}",
         }
     ]
 }
-
 r = requests.post(url, json = request)
