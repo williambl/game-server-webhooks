4587bee7ec6deb27852610067a040adb26ecaed9
williambl <william@williambl.com>
2018-07-06 14:21:38 +0100

    Initial Commit

---
+++ push-webhook.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+url=""
+
+request="
+{
+    \"name\": \"\",
+    \"channel_id\": \"\",
+    \"token\": \"\",
+    \"avatar\": null,
+    \"guild_id\": \"\",
+    \"id\": \"\",
+    \"user\": {
+        \"username\": \"\",
+        \"discriminator\": \"\",
+        \"id\": \"\",
+        \"avatar\": \"\"
+    }
+}"
+
+echo $request
