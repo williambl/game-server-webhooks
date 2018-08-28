b001dab385dca1b0706b761862c015d8e1482d05
williambl <william@williambl.com>
2018-08-28 22:41:53 +0100

    Add game and image from argument in generate-message.py

--- generate-message.py
+++ generate-message.py
@@ -1,8 +1,15 @@
 import netifaces as ni
 import subprocess
+import sys
 
-game = ""
-image = ""
+game = sys.argv[1]
+
+if (game == "Garry's Mod Sandbox"):
+    image = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Gmodlogo.svg/480px-Gmodlogo.svg.png"
+elif (game == "Garry's Mod Battle Royale"):
+    image = "https://steamuserimages-a.akamaihd.net/ugc/279595726542479855/0FA7121E5843B345BC171845C3FB2D80BC13758D/"
+else:
+    image = ""
 
 ni.ifaddresses('eth0')
 ip = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
