16b1ca6b6573e2f7508caed0c174f5f7b9a4e49c
williambl <william@williambl.com>
2018-10-26 15:43:47 +0100

    Merge push-webhook.py into generate-message.py

--- generate-message.py
+++ generate-message.py
@@ -2,34 +2,80 @@ import netifaces as ni
 import subprocess
 import sys
 import json
+import requests
+import argparse
+import os
 
-game = sys.argv[1]
-message = sys.argv[3]
+ # set up variables from input
+def setup_variables():
+    url = os.getenv("GAME_WEBHOOK_URL")
 
-if (len(sys.argv) > 2):
-    redirect_to_game = sys.argv[2] == "redirect"
-else:
-    redirect_to_game = False
+    game = sys.argv[1]
+    redirect = "redirect" in sys.argv
+    message = sys.argv[3]
 
-with open("games.json", "r") as f:
-    games = json.load(f)
+    with open("games.json", "r") as f:
+        games = json.load(f)
 
-image = games.get(game, "")
+    return url, game, redirect, message, games
 
-ni.ifaddresses('eth0')
-ip = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
+def set_game_image(game_list, game_name):
+    return game_list.get(game_name, "")
 
-if (redirect_to_game):
-    link = f"http://{ip}:8080"
-else:
-    link = ""
+def get_ip_address():
+    ni.ifaddresses('eth0')
+    return ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
 
-process = ["python3.6", "push-webhook.py", "--game", game, "--link", link, "--addr", ip, "--image", image]
-if (message != None):
-    process.append("--message")
-    process.append(message)
+def set_link(redirect):
+    if (redirect):
+        return f"http://{ip}:8080"
+    else:
+        return ""
 
-subprocess.run(process)
+def create_request(url, game, message, image, ip):
+    request = {
+        "text": "New server started!",
+        "attachments": [
+            {
+                "fallback": f"New {game} server started at {ip}.",
+                "title": f"{game} server started!",
+                "title_link": f"{url}",
+                "text": f"{message}",
+                "color": "#fa8423",
+                "fields": [
+                    {
+                        "title": "Game",
+                        "value": f"{game}",
+                        "short": True
+                    },
+                    {
+                        "title": "Address",
+                        "value": f"{ip}",
+                        "short": True
+                    }
+                ],
+                "actions": [
+                    {
+                        "name": "Join Game",
+                        "text": "Join",
+                        "url": f"{url}",
+                        "style": "primary"
+                    }
+                ],
+                "image_url": f"{image}"
+            }
+        ]
+    }
+    return request
 
-if (redirect_to_game):
-    subprocess.run(["python3.6", "redirect-to-game.py", ip])
+def main():
+    url, game, redirect, message, games = setup_variables()
+    image = set_game_image(games, game)
+    ip = get_ip_address()
+    link = set_link(redirect)
+
+    request = create_request(url, game, message, image, ip)
+    r = requests.post(url, json = request)
+
+if __name__ == "__main__":
+    main()
